876a9ece1d39e37eb1bfd5e9b4cbffaa91fea61b
Laider Lucas <laiderlucas@hotmail.com>
2021-02-23 19:38:07 -0400

    dia 01  - criando ambiente

---
+++ NLW_4/node/api/src/server.ts
@@ -0,0 +1,9 @@
+import express, { request, response } from 'express';
+
+const app = express();
+
+app.get("/", (request, response) =>{
+    return response.send("Hello World!");
+})
+
+app.listen(3333, () => {console.log("Server is running!")})
