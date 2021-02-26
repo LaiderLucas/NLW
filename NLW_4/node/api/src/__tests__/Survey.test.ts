09732a6e1727dab65e2a367d6f98d0721a355ae5
Laider Lucas <laiderlucas@hotmail.com>
2021-02-25 22:31:00 -0400

    dia 3 - criado surveys e adicinado testes

---
+++ NLW_4/node/api/src/__tests__/Survey.test.ts
@@ -0,0 +1,34 @@
+import request from 'supertest';
+import { app } from '../app';
+
+import createConnection from '../database'
+
+
+describe("Survey", () => {
+    beforeAll( async() => {
+        const connection = await createConnection();
+        await connection.runMigrations()
+    });
+
+    it("Should be able to create a new Survey", async () => {
+        const response = await request(app).post("/surveys")
+        .send({
+            title: "Title Exemple",
+            description:"Descripion Exemple",
+        })
+        expect(response.status).toBe(201)
+        expect(response.body).toHaveProperty("id")
+    })
+
+    it("Should be able to get all surveys", async () => {
+        await request(app).post("/surveys").send({
+            title: "Title Exemple2",
+            description:"Descripion Exemple2",
+        });
+
+        const response = await request(app).get("/surveys");
+
+        expect(response.body.length).toBe(2)
+    })
+    
+})
--- NLW_4/node/api/src/server.ts
+++ NLW_4/node/api/src/server.ts
@@ -1,13 +1,3 @@
-import "reflect-metadata"
-import express, { request, response } from 'express';
-import "./database"
-import { router } from "./routers";
-
-const app = express();
-
-app.use(express.json());
-
-app.use(router);
-
+import { app } from "./app"
 
 app.listen(3333, () => {console.log("Server is running!")})
