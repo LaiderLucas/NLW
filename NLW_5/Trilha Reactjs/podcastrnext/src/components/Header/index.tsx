b598b315951b88f894713c2ef962818b5e499d2b
Laider Lucas <laiderlucas@hotmail.com>
2021-05-22 15:08:21 -0400

    add dark mode

--- NLW_5/Trilha Reactjs/podcastrnext/src/components/Header/index.tsx
+++ NLW_5/Trilha Reactjs/podcastrnext/src/components/Header/index.tsx
@@ -1,19 +1,44 @@
-import format from 'date-fns/format'
-import ptBR from 'date-fns/locale/pt-BR'
-import styles from './styles.module.scss';
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR";
+import styles from "./styles.module.scss";
+import React, { useState } from "react";
+import Switch from "react-switch";
+import Link from "next/link";
 
 export function Header() {
+  const [checked, setChecked] = useState(false);
+  const handleChange = (nextChecked) => {
+    setChecked(nextChecked);
+    if (nextChecked) {
+      document.getElementById("podcastr").className = "dark-mode";
+    } else {
+      document.getElementById("podcastr").className = "";
+    }
+  };
+  const currentDate = format(new Date(), "EEEEEE, d MMMM", {
+    locale: ptBR,
+  });
+  return (
+    <header className={styles.headerContainer}>
+      <Link href={"/"}>
+        <img src="/logo.svg" alt="Podcastr" />
+      </Link>
+      <p>O melhor para você ouvir, sempre</p>
 
-    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
-        locale: ptBR,
-    })
-    return (
-        <header className={styles.headerContainer}>
-            <img src="/logo.svg" alt="Podcastr"/>
-
-            <p>O melhor para você ouvir, sempre</p>
-
-            <span>{currentDate}</span>
-        </header>
-    );
+      <span>{currentDate}</span>
+      <div className={styles.darkModeSwitch}>
+        <span>Dark Mode</span>
+        <Switch
+          onChange={handleChange}
+          checked={checked}
+          height={16}
+          width={40}
+          handleDiameter={10}
+          onColor={"#8257E5"}
+          activeBoxShadow={'0 0 2px 3px #04d361'}
+          className={styles.switch}
+        ></Switch>
+      </div>
+    </header>
+  );
 }
--- NLW_5/Trilha Reactjs/podcastrnext/src/pages/index.tsx
+++ NLW_5/Trilha Reactjs/podcastrnext/src/pages/index.tsx
@@ -127,7 +127,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 export const getStaticProps: GetStaticProps = async () => {
   const { data } = await api.get('episodes', {
     params: {
-      _limit: 12,
+      // _limit: 12,
       _sort: 'published_at',
       _order: 'desc'
     }
